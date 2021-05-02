58184f82f93326410fe1fd1e5235dd14a12b134e
FanDady <1140021702@qq.com>
2021-05-02 14:53:15 +0800

    Site updated: 2021-05-02 14:52:52

--- js/matery.js
+++ js/matery.js
@@ -1,3 +1,19 @@
+// 深色模式设置 
+function switchNightMode() 
+{ 
+    var body = document.body; 
+    if(body.classList.contains('dark')){ 
+        document.body.classList.remove('dark'); 
+        localStorage.setItem('dark','0'); 
+        $('#nightMode').removeClass("fa-lightbulb-o").addClass("fa-moon-o"); 
+        return; 
+    } else { 
+        document.body.classList.add('dark'); 
+        localStorage.setItem('dark','1'); 
+        $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb-o"); 
+        return; 
+    } 
+}
 $(function () {
     /**
      * 添加文章卡片hover效果.
@@ -168,5 +184,7 @@ $(function () {
     });
 
     // 初始化加载 tooltipped.
-    $('.tooltipped').tooltip();
-});
+    $('.tooltipped').tooltip();  
+}
+);
+
